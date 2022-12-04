9d862847fbc722fbe5d489408f2b31f40c4c1bbd
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-04 11:57:34 +0000

    Day 1

---
+++ 01_1/Program.cs
@@ -0,0 +1,17 @@
+﻿var count = 0;
+var prev = int.MaxValue;
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var curr = int.Parse(line);
+
+    if (curr > prev)
+    {
+        count++;
+    }
+
+    prev = curr;
+}
+
+Console.WriteLine(count);
---
+++ 01_2/Program.cs
@@ -0,0 +1,24 @@
+﻿var count = 0;
+var prev = int.MaxValue;
+var group = new List<int>();
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    group.Add(int.Parse(line));
+
+    if (group.Count == 3)
+    {
+        var curr = group.Sum();
+
+        if (curr > prev)
+        {
+            count++;
+        }
+        
+        prev = curr;
+        group.RemoveAt(0);
+    }
+}
+
+Console.WriteLine(count);
