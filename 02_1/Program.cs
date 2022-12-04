eb11c8d227bdb68936eb2cd3e69cc0c70bc97a62
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-04 12:16:14 +0000

    Day 2

---
+++ 02_1/Program.cs
@@ -0,0 +1,23 @@
+﻿var x = 0;
+var z = 0;
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var v = int.Parse(line.Split(' ')[1]);
+
+    switch (line[0])
+    {
+        case 'f':
+            x += v;
+            break;
+        case 'd':
+            z += v;
+            break;
+        case 'u':
+            z -= v;
+            break;
+    }
+}
+
+Console.WriteLine(x * z);
---
+++ 02_2/Program.cs
@@ -0,0 +1,25 @@
+﻿var x = 0;
+var z = 0;
+var r = 0;
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var v = int.Parse(line.Split(' ')[1]);
+
+    switch (line[0])
+    {
+        case 'f':
+            x += v;
+            z += r * v;
+            break;
+        case 'd':
+            r += v;
+            break;
+        case 'u':
+            r -= v;
+            break;
+    }
+}
+
+Console.WriteLine(x * z);
