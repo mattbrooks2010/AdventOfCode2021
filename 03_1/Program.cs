b3638c9e04b5d27b020bf7d6a08054b23804ab77
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-06 05:29:41 +0000

    Day 3

---
+++ 03_1/Program.cs
@@ -0,0 +1,19 @@
+﻿var hist = new int[12];
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    for (var i = 0; i < line.Length; i++)
+    {
+        hist[i] += line[i] == '1' ? 1 : -1;
+    }
+}
+
+hist = hist
+    .Select(v => v > 0 ? 1 : 0)
+    .ToArray();
+
+var gamma = Convert.ToInt32(string.Join(string.Empty, hist), 2);
+var epsilon = gamma ^ 4095;
+
+Console.WriteLine(gamma * epsilon);
---
+++ 03_2/Program.cs
@@ -0,0 +1,46 @@
+﻿var diag = new List<string>();
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    diag.Add(line);
+}
+
+int GetRating(Func<int, bool> bitComparer)
+{
+    // Don't mutate original collection
+    var data = diag.ToList();
+
+    for (var i = 0; i < 12; i++)
+    {
+        var hist = 0;
+
+        foreach (var line in data)
+        {
+            hist += line[i] == '1' ? 1 : -1;
+        }
+
+        var common = bitComparer(hist) ? '1' : '0';
+
+        for (var j = 0; j < data.Count; j++)
+        {
+            if (common != data[j][i])
+            {
+                data.RemoveAt(j);
+                j -= 1;
+            }
+        }
+
+        if (data.Count == 1)
+        {
+            break;
+        }
+    }
+
+    return Convert.ToInt32(string.Join(string.Empty, data[0]), 2);
+}
+
+var o2 = GetRating(h => h >= 0);
+var co2 = GetRating(h => h < 0);
+
+Console.WriteLine(o2 * co2);
