00df0fbdf37e870fad8e2a396741cd6521ecef90
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-11 21:32:55 +0000

    Day 5

---
+++ 05_1/Program.cs
@@ -0,0 +1,49 @@
+﻿var coords = new int[1000, 1000];
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var parts = line.Split(" -> ");
+    var x1y1 = parts[0].Split(',');
+    var x2y2 = parts[1].Split(',');
+    var x1 = int.Parse(x1y1[0]);
+    var y1 = int.Parse(x1y1[1]);
+    var x2 = int.Parse(x2y2[0]);
+    var y2 = int.Parse(x2y2[1]);
+
+    if (x1 == x2 || y1 == y2)
+    {
+        if (x1 > x2 || y1 > y2)
+        {
+            var x1tmp = x1;
+            var y1tmp = y1;
+            x1 = x2;
+            y1 = y2;
+            x2 = x1tmp;
+            y2 = y1tmp;
+        }
+
+        for (var x = x1; x <= x2; x++)
+        {
+            for (var y = y1; y <= y2; y++)
+            {
+                coords[x, y]++;
+            }
+        }
+    }
+}
+
+var overlaps = 0;
+
+for (var i = 0; i < coords.GetLength(0); i++)
+{
+    for (var j = 0; j < coords.GetLength(1); j++)
+    {
+        if (coords[i, j] >= 2)
+        {
+            overlaps++;
+        }
+    }
+}
+
+Console.WriteLine(overlaps);
---
+++ 05_2/Program.cs
@@ -0,0 +1,40 @@
+﻿var coords = new int[1000, 1000];
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var parts = line.Split(" -> ");
+    var x1y1 = parts[0].Split(',');
+    var x2y2 = parts[1].Split(',');
+    var x1 = int.Parse(x1y1[0]);
+    var y1 = int.Parse(x1y1[1]);
+    var x2 = int.Parse(x2y2[0]);
+    var y2 = int.Parse(x2y2[1]);
+
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    var len = Math.Max(Math.Abs(dx), Math.Abs(dy));
+
+    for (var i = 0; i <= len; i++)
+    {
+        var x = x1 + (i * Math.Sign(dx));
+        var y = y1 + (i * Math.Sign(dy));
+
+        coords[x, y]++;
+    }
+}
+
+var overlaps = 0;
+
+for (var i = 0; i < coords.GetLength(0); i++)
+{
+    for (var j = 0; j < coords.GetLength(1); j++)
+    {
+        if (coords[i, j] >= 2)
+        {
+            overlaps++;
+        }
+    }
+}
+
+Console.WriteLine(overlaps);
