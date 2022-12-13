263fbaa14a978d228c3a192108fef0589fb58a9b
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-13 13:02:35 +0000

    Day 6

---
+++ 06_1/Program.cs
@@ -0,0 +1,27 @@
+﻿var fish = new List<int>();
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    fish.AddRange(line.Split(',').Select(s => int.Parse(s)));
+}
+
+for (var day = 1; day <= 80; day++)
+{
+    var len = fish.Count;
+
+    for (var i = 0; i < len; i++)
+    {
+        if (fish[i] == 0)
+        {
+            fish[i] = 6;
+            fish.Add(8);
+        }
+        else
+        {
+            fish[i]--;
+        }
+    }
+}
+
+Console.WriteLine(fish.Count);
---
+++ 06_2/Program.cs
@@ -0,0 +1,37 @@
+﻿var fish = new ulong[9];
+var count = 0UL;
+
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    foreach (var t in line.Split(',').Select(s => int.Parse(s)))
+    {
+        fish[t]++;
+        count++;
+    }
+}
+
+for (var i = 0; i < 256; i++)
+{
+    var @new = fish[0];
+    count += @new;
+
+    for (var j = 0; j < fish.Length; j++)
+    {
+        if (j == 6)
+        {
+            fish[j] = fish[j + 1] + @new;
+        }
+        else if (j == 8)
+        {
+            fish[j] = @new;
+        }
+        else
+        {
+            fish[j] = fish[j + 1];
+        }
+    }
+}
+
+Console.WriteLine(count);
